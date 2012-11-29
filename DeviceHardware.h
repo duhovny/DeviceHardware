533a006eac573a9e3d54414a84d921c1996e00b3
Sergey Filippov <duhovny@gmail.com>
2012-11-29 13:15:45 -0500

    Add files

---
+++ DeviceHardware.h
@@ -0,0 +1,84 @@
+//
+//  DeviceHardware.h
+//
+//  Used to determine version of device
+//  this software is running on.
+//
+//  You can get:
+//
+//  - Specific platform (ex.: iPad 2 GSM)
+//  - General Platform  (ex.: iPad 2)
+//  - Platform Type		(ex.: iPad)
+//  - Platform String	(ex.: "iPad 2 (GSM)")
+//
+//  Take care,
+//  Bowyer Worldwide
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+	DeviceHardwareSpecificPlatform_Unknown,
+	DeviceHardwareSpecificPlatform_iPhone_1G,
+	DeviceHardwareSpecificPlatform_iPhone_3G,
+	DeviceHardwareSpecificPlatform_iPhone_3GS,
+	DeviceHardwareSpecificPlatform_iPhone_4,
+	DeviceHardwareSpecificPlatform_iPhone_4_Verizon,
+	DeviceHardwareSpecificPlatform_iPhone_4S,
+	DeviceHardwareSpecificPlatform_iPhone_5,
+	DeviceHardwareSpecificPlatform_iPod_Touch_1G,
+	DeviceHardwareSpecificPlatform_iPod_Touch_2G,
+	DeviceHardwareSpecificPlatform_iPod_Touch_3G,
+	DeviceHardwareSpecificPlatform_iPod_Touch_4G,
+	DeviceHardwareSpecificPlatform_iPod_Touch_5G,
+	DeviceHardwareSpecificPlatform_iPad,
+	DeviceHardwareSpecificPlatform_iPad_2_GSM,
+	DeviceHardwareSpecificPlatform_iPad_2_CDMA,
+	DeviceHardwareSpecificPlatform_iPad_2_WiFi,
+	DeviceHardwareSpecificPlatform_iPad_3_WiFi,
+	DeviceHardwareSpecificPlatform_iPad_3_GSM,
+	DeviceHardwareSpecificPlatform_iPad_3_CDMA,
+	DeviceHardwareSpecificPlatform_iPad_4_WiFi,
+	DeviceHardwareSpecificPlatform_iPad_Mini_WiFi,
+	DeviceHardwareSpecificPlatform_Simulator
+} DeviceHardwareSpecificPlatform;
+
+typedef enum {
+	DeviceHardwareGeneralPlatform_Unknown,
+	DeviceHardwareGeneralPlatform_iPhone_1G,
+	DeviceHardwareGeneralPlatform_iPhone_3G,
+	DeviceHardwareGeneralPlatform_iPhone_3GS,
+	DeviceHardwareGeneralPlatform_iPhone_4,
+	DeviceHardwareGeneralPlatform_iPhone_4S,
+	DeviceHardwareGeneralPlatform_iPhone_5,
+	DeviceHardwareGeneralPlatform_iPod_Touch_1G,
+	DeviceHardwareGeneralPlatform_iPod_Touch_2G,
+	DeviceHardwareGeneralPlatform_iPod_Touch_3G,
+	DeviceHardwareGeneralPlatform_iPod_Touch_4G,
+	DeviceHardwareGeneralPlatform_iPod_Touch_5G,
+	DeviceHardwareGeneralPlatform_iPad,
+	DeviceHardwareGeneralPlatform_iPad_2,
+	DeviceHardwareGeneralPlatform_iPad_3,
+	DeviceHardwareGeneralPlatform_iPad_4,
+	DeviceHardwareGeneralPlatform_iPad_Mini,
+	DeviceHardwareGeneralPlatform_Simulator
+} DeviceHardwareGeneralPlatform;
+
+typedef enum {
+	DeviceHardwarePlatformType_Unknown,
+	DeviceHardwarePlatformType_iPhone,
+	DeviceHardwarePlatformType_iPod_Touch,
+	DeviceHardwarePlatformType_iPad,
+	DeviceHardwarePlatformType_Simulator
+} DeviceHardwarePlatformType;
+
+@interface DeviceHardware : NSObject
+
++ (DeviceHardwareSpecificPlatform)specificPlatform;
++ (DeviceHardwareGeneralPlatform)generalPlatform;
++ (DeviceHardwareGeneralPlatform)platformType;
++ (NSString *)platformString;
+
++ (NSString *)machine;
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 DeviceHardware
 ==============
 
-Used to determine version of device.
+Determines iOS device version.
+
+```objective-c
+// Usage example, choose whatever you like.
+
+DeviceHardwareSpecificPlatform deviceHardwareSpecificPlatform = [DeviceHardware specificPlatform];
+	DeviceHardwareGeneralPlatform deviceHardwareGeneralPlatform = [DeviceHardware generalPlatform];
+	DeviceHardwarePlatformType deviceHardwarePlatformType = [DeviceHardware platformType];
+	NSString *platformString = [DeviceHardware platformString];
+```
